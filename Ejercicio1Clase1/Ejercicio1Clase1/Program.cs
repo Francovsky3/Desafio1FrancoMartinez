21276340a9d8e4480ffa6c07341694d884536d62
Francovsky3 <francom9000@gmail.com>
2023-12-18 18:51:39 -0300

    Desafio 1 de la primera clase
    Nombre: Franco Martinez

---
+++ Ejercicio1Clase1/Ejercicio1Clase1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Declaracion de Variables
+
+string Nombre = "Franco";
+string Apellido = "Martinez";
+float DNI = 43628627;
+int Edad = 21;
+double Altura = 1.76;
+
+//Cuerpo de programa o instrucciones
+//Actividad 1
+Console.WriteLine("Nombre: " + Nombre);
+Console.WriteLine("Apellido: " + Apellido);
+Console.WriteLine("DNI: " + DNI);
+Console.WriteLine("Edad: " + Edad);
+Console.WriteLine("Altura: " + Altura);
+
+//Actividad 3
+Console.WriteLine("Mi nombre es: " + Nombre + " Francovsky " + Apellido);
